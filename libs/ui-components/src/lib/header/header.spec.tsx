10a2dba4936c394d4d1c937bfa58d3eb5f2b19f0
Denys Pavlenko <denys.pavlenko@roamler.com>
2022-07-16 17:35:59 +0300

    Create header tests

--- libs/ui-components/src/lib/header/header.spec.tsx
+++ libs/ui-components/src/lib/header/header.spec.tsx
@@ -1,10 +1,42 @@
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './header';
 
+const MockLogo = () => <svg data-testid="logo"></svg>;
+const mockNavigation = [
+  {
+    title: 'Dashboard',
+    path: '/',
+  },
+  {
+    title: 'Settings',
+    path: '/settings',
+  },
+];
+const mockAvatar = '/user.jpg';
+
 describe('Header', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Header />);
-    expect(baseElement).toBeTruthy();
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Header
+          logo={MockLogo}
+          navigation={mockNavigation}
+          avatar={mockAvatar}
+        />
+      </BrowserRouter>
+    );
+  });
+  it('should render logo', () => {
+    const logo = screen.getByTestId('logo');
+    expect(logo).toBeInTheDocument();
+  });
+  it('should render navigation', () => {
+    const dashboardLink = screen.getByText('Settings');
+    expect(dashboardLink).toBeInTheDocument();
+  });
+  it('should render avatar', () => {
+    const image = screen.getByAltText('user');
+    expect(image).toBeInTheDocument();
   });
 });
