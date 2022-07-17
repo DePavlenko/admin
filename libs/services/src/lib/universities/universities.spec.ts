70d20d547e074bcf86a29975961ddadd877f6d1b
Denys Pavlenko <denys.pavlenko@roamler.com>
2022-07-17 13:17:57 +0300

    test(universities.spec.ts): implement API tests

---
+++ libs/services/src/lib/universities/universities.spec.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { UniversitiesService } from './universities';
+import { University } from '@roamlerorg/types';
+
+const universitiesService = new UniversitiesService();
+
+const universities: University[] = [
+  {
+    country: 'France',
+    domains: ['AgroParisTech'],
+    web_pages: ['http://testwebpage.com'],
+  },
+  {
+    country: 'France',
+    domains: ['American Graduate School in Paris'],
+    web_pages: ['http://testwebpage.com'],
+  },
+];
+
+describe('universitiesService', () => {
+  let mock: MockAdapter;
+
+  beforeAll(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.reset();
+  });
+
+  it('should return universities list', async () => {
+    mock
+      .onGet('http://universities.hipolabs.com/search?country=France')
+      .reply(200, universities);
+    const result = await universitiesService.getUniversities('France');
+    expect(result).toEqual(universities);
+  });
+});
